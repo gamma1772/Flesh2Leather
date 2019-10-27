28eecbd253d8eb861ae85eebcee7f8f8c87e0e82
Codename Revy <codenamerevy@gmail.com>
2019-10-27 18:21:00 +0100

    0.2.1 Update

--- src/main/java/com/codenamerevy/flesh2leather/Flesh2Leather.java
+++ src/main/java/com/codenamerevy/flesh2leather/Flesh2Leather.java
@@ -2,6 +2,7 @@ package com.codenamerevy.flesh2leather;
 
 import com.codenamerevy.flesh2leather.config.CommonConfig;
 import com.codenamerevy.flesh2leather.util.Reference;
+import com.codenamerevy.flesh2leather.util.handler.EventHandler;
 import com.codenamerevy.flesh2leather.util.handler.RegistryHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
@@ -17,6 +18,7 @@ public class Flesh2Leather
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
         MinecraftForge.EVENT_BUS.addListener(RegistryHandler::onItemRegistry);
+        MinecraftForge.EVENT_BUS.addListener(EventHandler::registerRecipeSerializers);
         MinecraftForge.EVENT_BUS.register(this);
 
         CommonConfig.loadConfig(CommonConfig.COMMON_CONFIG, FMLPaths.CONFIGDIR.get().resolve("f2l-common.toml"));
@@ -24,6 +26,7 @@ public class Flesh2Leather
     private void setup (final FMLCommonSetupEvent event)
     {
         MinecraftForge.EVENT_BUS.register(new RegistryHandler());
+        MinecraftForge.EVENT_BUS.register(new EventHandler());
         Reference.LOGGER.info(Reference.MODNAME + " with ID " + Reference.MODID + " started FMLCommonSetup event.");
     }
 }
---
+++ src/main/java/com/codenamerevy/flesh2leather/util/handler/EventHandler.java
@@ -0,0 +1,18 @@
+package com.codenamerevy.flesh2leather.util.handler;
+
+import com.codenamerevy.flesh2leather.items.ItemConditions;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class EventHandler
+{
+    @SubscribeEvent
+    public static void registerRecipeSerializers(RegistryEvent<IRecipeSerializer<?>> event)
+    {
+        CraftingHelper.register(ItemConditions.Serializer.INSTANCE);
+    }
+}
