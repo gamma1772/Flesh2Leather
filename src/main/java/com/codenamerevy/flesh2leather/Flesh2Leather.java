3e30a083f6ad74e85da74fe7a8b79ead6cd596e3
CodenameRevy <codenamerevy@gmail.com>
2020-10-07 15:26:14 +0200

    1.0.2 (for 1.15.2)

--- src/main/java/com/codenamerevy/flesh2leather/Flesh2Leather.java
+++ src/main/java/com/codenamerevy/flesh2leather/Flesh2Leather.java
@@ -1,33 +1,50 @@
+/**
+ * Copyright 2020 Marko Dujović
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.codenamerevy.flesh2leather;
 
-import com.codenamerevy.flesh2leather.config.Config;
-import com.codenamerevy.flesh2leather.util.Ref;
-import com.codenamerevy.flesh2leather.util.handler.EventHandler;
-import com.codenamerevy.flesh2leather.util.handler.RegistryHandler;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 
-@Mod(Ref.MODID)
+@Mod("flesh2leather")
 public class Flesh2Leather
 {
+    private static final String MODID = "flesh2leather";
+
+    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    private static final RegistryObject<Item> COMBINED_FLESH = ITEMS.register("combined_flesh", () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
+
     public Flesh2Leather()
     {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_CONFIG);
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        MinecraftForge.EVENT_BUS.addListener(Config::onLoad);
-        MinecraftForge.EVENT_BUS.addListener(Config::onFileChange);
-
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+        ITEMS.register(modEventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
-    }
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-        MinecraftForge.EVENT_BUS.register(new RegistryHandler());
-        MinecraftForge.EVENT_BUS.register(new EventHandler());
     }
 }
