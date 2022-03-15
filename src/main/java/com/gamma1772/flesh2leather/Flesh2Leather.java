d70a2168fbbc80f6fb5d389ebb65b59d99b9a1fb
Marko Dujović <dujovicm@outlook.com>
2022-03-15 21:30:10 +0100

    1.18.2

--- src/main/java/com/gamma1772/flesh2leather/Flesh2Leather.java
+++ src/main/java/com/gamma1772/flesh2leather/Flesh2Leather.java
@@ -1,14 +1,14 @@
-package com.codenamerevy.flesh2leather;
+package com.gamma1772.flesh2leather;
 
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("flesh2leather")
 public class Flesh2Leather
@@ -16,7 +16,7 @@ public class Flesh2Leather
     private static final String MODID = "flesh2leather";
 
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    private static final RegistryObject<Item> COMBINED_FLESH = ITEMS.register("combined_flesh", () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
+    private static final RegistryObject<Item> COMBINED_FLESH = ITEMS.register("combined_flesh", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
 
     public Flesh2Leather()
     {
