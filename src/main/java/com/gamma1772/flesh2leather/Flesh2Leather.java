4fba62b212642e3ea319e8d29614d500d53c29ff
Marko Dujović <dujovicm@outlook.com>
2023-01-16 13:01:17 +0100

    Add Creative Tab injection event

--- src/main/java/com/gamma1772/flesh2leather/Flesh2Leather.java
+++ src/main/java/com/gamma1772/flesh2leather/Flesh2Leather.java
@@ -1,9 +1,14 @@
 package com.gamma1772.flesh2leather;
 
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -16,7 +21,7 @@ public class Flesh2Leather
     private static final String MODID = "flesh2leather";
 
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    private static final RegistryObject<Item> COMBINED_FLESH = ITEMS.register("combined_flesh", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+    private static final RegistryObject<Item> COMBINED_FLESH = ITEMS.register("combined_flesh", () -> new Item(new Item.Properties()));
 
     public Flesh2Leather()
     {
@@ -25,5 +30,13 @@ public class Flesh2Leather
         ITEMS.register(modEventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.register(this);
+    }
+
+    @SubscribeEvent
+    public void addToTab(CreativeModeTabEvent.BuildContents event) {
+        if (event.getTab() == CreativeModeTabs.INGREDIENTS) {
+            event.getEntries().putAfter(Items.LEATHER.getDefaultInstance(), COMBINED_FLESH.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        }
     }
 }
